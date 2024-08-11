d0728ec6278019a40137e8241678cec8412a4e19
jmmount <eslnyc.jason@gmail.com>
2024-08-11 11:50:41 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar.js';
+//import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Universal English
         </p>
         <a
           className="App-link"
@@ -15,11 +15,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Currently under construction.
         </a>
       </header>
     </div>
   );
-}
+}*/
+
+function App() {
+  return <Navbar/>
+};
 
 export default App;

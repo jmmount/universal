6ed7270baff37ef2e5285484add74883cf813a20
jmmount <eslnyc.jason@gmail.com>
2024-08-27 07:26:56 -0400

    updates home

--- src/components/ImgCard.jsx
+++ src/components/ImgCard.jsx
@@ -1,20 +1,26 @@
-const ImgCard = ({ image, title, body }) => {
-    return (
-        <div className="card mb-3" style={{maxWidth: '540px'}}>
+import NavigateButton from "./NavigateButton";
+
+const ImgCard = ({ image, title, body, to, buttonText }) => {
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="card mb-3" style={{ maxWidth: "1140px" }}>
         <div className="row g-0">
           <div className="col-md-4">
-            <img src={image} className="img-fluid rounded-start" alt={title}/>
+            <img src={image} className="img-fluid rounded-start" alt={title} />
           </div>
           <div className="col-md-8">
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{body}</p>
-              <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+              <NavigateButton className="btn btn-outline-success" to={to}>
+                {buttonText}
+              </NavigateButton>
             </div>
           </div>
         </div>
-      </div> 
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ImgCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,39 @@ import QuoteCard from "../components/QuoteCard.jsx";
 function Home() {
   return (
     <>
-      <h1 class="display-1">Summon the universe.
-      <small class="text-body-secondary">Join the over 1.35 billion people around the world that use the English language to trade, communicate, and express themselves. </small>
+      <h1 class="display-1">
+        Summon the universe.
+        <small class="text-body-secondary">
+          Join the over 1.35 billion people around the world that use the
+          English language for communication, businesss, and self-expression.{" "}
+        </small>
       </h1>
       <QuoteCard
         quoteText="The limits of my language mean the limits of my world."
         author="Ludwig Wittgenstein"
         ource="Tractatus logigo-philosphicus, 1922"
       />
-      <ImgCard/>
-      <ImgCard/>
-      <ImgCard/>
+      <ImgCard
+        image="/logo192.png"
+        title="Grammar"
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        to=""
+        buttonText="Learn More"
+      />
+      <ImgCard
+        image="/logo192.png"
+        title="Self-Study Course"
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        to=""
+        buttonText="Learn More"
+      />
+      <ImgCard
+        image="/logo192.png"
+        title="Progress Tracking"
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        to=""
+        buttonText="Learn More"
+      />
     </>
   );
 }
